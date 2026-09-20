3d4f6ff94b6d6cf9ad1f09a03b43ff489f127ea0
chmu7568 <christian.mueller@esolutions.de>
2026-09-20 02:14:49 +0200

    add tutorial

--- game/levels/tutorial/tutorial_level.gd
+++ game/levels/tutorial/tutorial_level.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
-@onready var start_button: Button = $StartButton
-@onready var animation: AnimationPlayer = $Animation
+@onready var animation: AnimationPlayer = $Intro/Animation
+@onready var tutorial: Control = $Tutorial
+@onready
+var tutorial_pages: Array[Control] = [$Tutorial/Tutorial1, $Tutorial/Tutorial2, $Tutorial/Tutorial3]
+@onready var start_button: Button = $Tutorial/StartButton
+@onready var right_arrow: Control = $Tutorial/RightArrow
+@onready var left_arrow: Control = $Tutorial/LeftArrow
+
+var page = 0
 
 
 func _ready():
@@ -12,9 +19,35 @@ func _ready():
 	animation.animation_finished.connect(_on_intro_animation_finished)
 
 
+func _input(event: InputEvent):
+	if not tutorial.visible:
+		return
+
+	if event.is_action_pressed("move_right"):
+		if page < 3:
+			page += 1
+			_update_tutorial_page()
+
+	if event.is_action_pressed("move_left"):
+		if page > 0:
+			page -= 1
+			_update_tutorial_page()
+
+
 func _on_start_pressed():
 	SceneManager.load_game_scene()
 
 
 func _on_intro_animation_finished(_animation):
-	start_button.show()
+	$Intro.hide()
+	tutorial.show()
+	_update_tutorial_page()
+
+
+func _update_tutorial_page():
+	left_arrow.visible = page != 0
+	tutorial_pages[0].visible = page == 0
+	tutorial_pages[1].visible = page == 1
+	tutorial_pages[2].visible = page == 2
+	right_arrow.visible = page != 3
+	start_button.visible = page == 3
